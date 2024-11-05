613913fc7794bb6526697c873eb212348e117117
Anhtt9x <anhtt454598@gmail.com>
2024-11-05 21:42:09 +0700

    package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Anhtt9x",
+    author_email="anhtt454598@gmail.com",
+    packages=find_packages()
+)
